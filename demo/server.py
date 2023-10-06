88c30aa376e6ac5250b0bb14db1b1fef7356f2b6
Alexander <Alexander@MacBook-Pro-4.local>
2023-10-06 13:12:09 +0300

    dunno

--- demo/server.py
+++ demo/server.py
@@ -2,29 +2,36 @@ import os
 import sys
 import mimetypes
 
-from flask import Flask, send_file, abort
+from flask import Flask, Response, send_file, abort
 
 app = Flask(__name__)
-current_folder = os.path.dirname(os.getcwd())
-print('pwd', current_folder)
+BASE_DIR = os.path.dirname(os.getcwd())
+print('BASE_DIR', BASE_DIR)
 entry_file = 'test.html'
 
 @app.route('/<path:filename>', methods=['GET'])
 def serve_file(filename):
-    file_path = os.path.join(current_folder, filename)
-    print(file_path)
+	file_path = os.path.join(BASE_DIR, filename)
+	print(file_path)
 
-    if os.path.exists(file_path):
-        mime_type, _ = mimetypes.guess_type(file_path)
-        return send_file(file_path, mimetype=mime_type)
-    else:
-        abort(404, description="Resource not found")
+	if os.path.exists(file_path):
+		mime_type, _ = mimetypes.guess_type(file_path)
+		return send_file(file_path, mimetype=mime_type)
+	else:
+		abort(404, description="Resource not found")
 
 @app.route('/')
-def serve_index():
-    index_path = os.path.join(current_folder, entry_file)
-    return send_file(index_path, mimetype='text/html')
+def index():
+	demo_dir     = os.path.join(BASE_DIR, 'demo')
+	files        = os.listdir(demo_dir)
+	html_files   = [f for f in files if f.endswith('.html')]
+
+	html_content = '<h1>Index:</h1><ul>'
+	for html_file in html_files:
+		html_content += f'<li><a href="demo/{html_file}">{html_file}</a></li>'
+	html_content += '</ul>'
+
+	return Response(html_content, mimetype='text/html')
 
 if __name__ == '__main__':
-    entry_file = sys.argv[1]
-    app.run(debug=True)
+	app.run(debug=True)
--- ui/class.Form.js
+++ ui/class.Form.js
@@ -2,32 +2,36 @@ import Puer, {PuerComponent} from '../puer.js'
 
 
 class Form extends PuerComponent {
-    constructor(props) {
-        Puer.default(props, 'title',         'Form')
-        Puer.default(props, 'subtitle',      'Please fill out this form')
-        Puer.default(props, 'buttonCaption', 'Submit')
-        Puer.default(props, 'action',        '')
-        Puer.default(props, 'method',        'POST')
-        Puer.default(props, 'enctype',       'application/x-www-form-urlencoded')
-        super(props)
+	constructor(props) {
+		Puer.default(props, 'title',         'Form')
+		Puer.default(props, 'subtitle',      'Please fill out this form')
+		Puer.default(props, 'buttonCaption', 'Submit')
+		Puer.default(props, 'action',        '')
+		Puer.default(props, 'method',        'POST')
+		Puer.default(props, 'enctype',       'application/x-www-form-urlencoded')
+		super(props)
+		this.state.error = ''
+	}
 
-    }
-
-    render() {
-        return (
-            div({autocomplete: 'off'}, [
-                h1({text: this.props.title}),
-                p({text: this.props.subtitle}),
-                p('error form-error', {text: 'Form error occured'}),
-                form({action: this.props.action, method: this.props.method, enctype: this.props.enctype}, [
-                    ...this.children,
-                    p([
-                        button({type: 'submit', text: this.props.buttonCaption})
-                    ])
-                ])
-            ])
-        )
-    }
+	render() {
+		return (
+			div({autocomplete: 'off'}, [
+				h1 ({text: this.props.title}),
+				p  ({text: this.props.subtitle}),
+				p  ({text: this.state.error, class: 'error form-error'}),
+				form ({
+					action  : this.props.action,
+					method  : this.props.method,
+					enctype : this.props.enctype
+				}, [
+					... this.children,
+					p ([
+						button ({type: 'submit', text: this.props.buttonCaption})
+					])
+				])
+			])
+		)
+	}
 }
 
 
