5a15da690f4d162a16a82861fe3da455f7042ca1
Alexander <Alexander@MacBook-Pro-4.local>
2023-11-14 14:28:40 +0200

    Route path parser

--- core/class.PuerRouter.js
+++ core/class.PuerRouter.js
@@ -2,10 +2,34 @@ import Puer from './class.Puer.js'
 
 class _PathParser {
 	constructor() {
-		this._reset()
+		const _this = this
+		Object.getOwnPropertyNames(Object.getPrototypeOf(this))
+            .filter(method => method.startsWith('_parse'))
+            .forEach(method => {
+            	const f = this[method]
+                this[method] = (...args) => {
+                	this._in(method + ' [' + args.join(', ') + ']')
+                	let result = f.bind(_this)(...args)
+                	this._out(method + (result ? ' TRUE' : ' FALSE'))
+                	return result
+                }
+            })
 	}
+
 	/********************** PRIVATE ***********************/
+
+	_in(s) {
+		console.log('+ '.repeat(this.indent), '[', s)
+		this.indent += 1
+	}
+
+	_out(s) {
+		this.indent -= 1
+		console.log('- '.repeat(this.indent), ']', s)
+	}
+
 	_next() {
+		console.log(this.c)
 		this.n ++
 		this.c = this.s[this.n]
 	}
@@ -18,9 +42,13 @@ class _PathParser {
 		this.n = 0
 		this.o = {}
 
-		this.currentComponent = this.o
+		this.stack     = []
+		this.component = this.o
+		this.indent    = 0
 	}
+
 	/********************** GETTER ***********************/
+
 	_getAlpha() {
 		let s = ''
 		while ('abcdefghijklmnopqrstuvwxyz0123456789'.includes(this.c)) {
@@ -29,10 +57,39 @@ class _PathParser {
 		}
 		return s
 	}
+
 	/********************** PARSER ***********************/
-	_parseSbOpen() {
-		if (this.c === '[') {
+
+	_parseChar(c) {
+		if (this.c === c) {
 			this._next()
+			return true
+		}
+		return false
+	}
+
+	_parseSbOpen() {
+		return this._parseChar('[')
+	}
+
+	_parseSbClose() {
+		return this._parseChar(']')
+	}
+
+	_parseCbOpen() {
+		return this._parseChar('{')
+	}
+
+	_parseCbClose() {
+		return this._parseChar('}')
+	}
+
+	_parseDot() {
+		return this._parseChar('.')
+	}
+
+	_parseProps() {
+		if (this._parseCbOpen()) {
 			return true
 		}
 		return false
@@ -40,10 +97,14 @@ class _PathParser {
 
 	_parseComponent() {
 		const componentName = this._getAlpha()
-		if (componentName) {
-			this.currentComponent[componentName] = {}
-			// this.currentComponent = this.currentComponent[componentName]
+		const component     = this.component
 
+		if (componentName) {
+			this.component[componentName] = {}
+			this.component = this.component[componentName]
+			this._parseProps()
+			this._parseComponents()
+			this.component = component
 			return true
 		}
 		return false
@@ -51,13 +112,23 @@ class _PathParser {
 
 	_parseComponents() {
 		if (this._parseSbOpen()) {
-			return true
-		} else if (this._parseComponent) {
+			while (this._parseComponent() && this._parseDot()) {
+				continue
+			}
+			if (this._parseSbClose()) {
+				return true
+			} else {
+				throw Error('Missing "]" at the end of components clause')
+			}
+		} else if (this._parseComponent()) {
+
 			return true
 		}
 		return false
 	}
+
 	/********************** PUBLIC ***********************/
+
 	parse(path) {
 		this._reset(path.toLowerCase())
 		this._parseComponents()
@@ -65,6 +136,7 @@ class _PathParser {
 	}
 }
 
+// #home[cargo{param:value}[mail{id:1321321}].system]
 
 class PuerRouter {
 	static instance = null
@@ -104,9 +176,9 @@ class PuerRouter {
 
 	_route(hash) {
 		this._pathDecode(hash)
-		let path = this._getPath(hash)
-		console.log(Puer.String.titleDivider(path.join('/'), 50, '-'))
-		this.app.__route(path)
+		// let path = this._getPath(hash)
+		// console.log(Puer.String.titleDivider(path.join('/'), 50, '-'))
+		// this.app.__route(path)
 	}
 
 	_routeComponent(c, path) {
