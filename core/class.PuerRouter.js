b334429002454589b654ff637f62829e9fcf6fa5
Oleksii Zaichenko <oleksii@platops.com>
2023-11-14 13:13:00 +0200

    started work on path parser

--- core/class.PuerRouter.js
+++ core/class.PuerRouter.js
@@ -1,5 +1,70 @@
 import Puer from './class.Puer.js'
 
+class _PathParser {
+	constructor() {
+		this._reset()
+	}
+	/********************** PRIVATE ***********************/
+	_next() {
+		this.n ++
+		this.c = this.s[this.n]
+	}
+
+	_reset(s=null) {
+		if (s) {
+			this.c = s[0]
+		}
+		this.s = s
+		this.n = 0
+		this.o = {}
+
+		this.currentComponent = this.o
+	}
+	/********************** GETTER ***********************/
+	_getAlpha() {
+		let s = ''
+		while ('abcdefghijklmnopqrstuvwxyz0123456789'.includes(this.c)) {
+			s += this.c
+			this._next()
+		}
+		return s
+	}
+	/********************** PARSER ***********************/
+	_parseSbOpen() {
+		if (this.c === '[') {
+			this._next()
+			return true
+		}
+		return false
+	}
+
+	_parseComponent() {
+		const componentName = this._getAlpha()
+		if (componentName) {
+			this.currentComponent[componentName] = {}
+			// this.currentComponent = this.currentComponent[componentName]
+
+			return true
+		}
+		return false
+	}
+
+	_parseComponents() {
+		if (this._parseSbOpen()) {
+			return true
+		} else if (this._parseComponent) {
+			return true
+		}
+		return false
+	}
+	/********************** PUBLIC ***********************/
+	parse(path) {
+		this._reset(path.toLowerCase())
+		this._parseComponents()
+		return this.o
+	}
+}
+
 
 class PuerRouter {
 	static instance = null
@@ -15,6 +80,15 @@ class PuerRouter {
 	}
 
 	/********************** PRIVATE ***********************/
+	_pathEncode(o) {
+
+	}
+
+	_pathDecode(path) {
+		const parser = new _PathParser()
+		console.log(parser.parse(path))
+	}
+
 	_getHash() {
 		return window.location.hash.split('#')[1]
 	}
@@ -29,9 +103,14 @@ class PuerRouter {
 	}
 
 	_route(hash) {
+		this._pathDecode(hash)
 		let path = this._getPath(hash)
 		console.log(Puer.String.titleDivider(path.join('/'), 50, '-'))
 		this.app.__route(path)
+	}
+
+	_routeComponent(c, path) {
+		// c.__
 	}
 
 	/*********************** PUBLIC ***********************/
