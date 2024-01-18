e90025bc3cb5fe9a96d682f617cf2ae7255bca08
Oleksii Zaichenko <o5zaichenko@gmail.com>
2024-01-18 16:26:56 +0200

    calendar in progress

--- ui/js/class.InputCalendar.js
+++ ui/js/class.InputCalendar.js
@@ -8,20 +8,48 @@ class InputCalendar extends FormInput {
 		super( ... args )
 		this.props.default('tagName', 'input')
 		this.props.default('type',    'text')
+		this.isShown   = false
+
+		this._totalWeeks = 6
+		this._daysOfWeek = 7
+		this._days       = Array.from(
+			{ length: this._totalWeeks },
+			() => new Array(this._daysOfWeek).fill(null)
+		)
+
+		this._calendar = null
 	}
 
 	_onClick(event) {
-		console.log('haha')
+		this.isShown = !this.isShown
+		this._calendar.toggle(this.isShown)
 	}
 
 	onInit() {
 		super.onInit()
 		this._on('click', this._onClick)
+		this._renderCalendar()
+	}
+
+	_renderCalendar() {
+		// const calendar = $.Box('calendar hidden')
+		for (let week = 0; week < this._totalWeeks; week++) {
+			for (let day = 0; day < this._daysOfWeek; day++) {
+				const dayCell = $.div('day-cell', {text: day})
+				this._days[week][day] = dayCell
+				this._calendar.append(dayCell)
+			}
+		}
+		// return calendar
+	}
+
+	_fillCalendar(year, month) {
+
 	}
 
 	render() {
-		const label = $.label('unselectable', {text: this.props.label})
-		this.children.push(label)
+		this._calendar = $.table('calendar hidden')
+		this.children.push(this._calendar)
 		return super.render()
 	}
 }
