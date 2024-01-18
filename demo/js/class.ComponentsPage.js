0247feeac85348b77191467348c111d5246b6045
TokensWork <116972181+TokensWork@users.noreply.github.com>
2024-01-18 11:31:22 +0200

    Added Button component to ComponentPage

--- demo/js/class.ComponentsPage.js
+++ demo/js/class.ComponentsPage.js
@@ -1,16 +1,27 @@
-import $ from '../../index.js'
+import $       from '../../index.js'
+import * as ui from '../../ui/index.js'
 
 
 export default class ComponentsPage extends $.Component {
+	onClick(event) {
+		alert('Hello World')
+	}
+
 	render() {
 		return $.Columns([
 			$.Rows('sidebar', [
+				$.Link({label: 'Button',   hash: 'cmp:button'   }),
 				$.Link({label: 'Calendar', hash: 'cmp:calendar' }),
 				$.Link({label: 'Checkbox', hash: 'cmp:checkbox' }),
 				$.Box({cssFlexGrow: 100})
 			]),
 			$.Box('body', [
-				$.Box({route: 'cmp:calendar', isDefaultRoute: true}, [
+				$.Box({route: 'cmp:button', isDefaultRoute: true}, [
+					$.Button('primary',   { text: 'Primary Button'   }),
+					$.Button('secondary', { text: 'Secondary Button' }),
+					$.Button('disabled',  { text: 'Disabled Button'  })
+				]),
+				$.Box({route: 'cmp:calendar'}, [
 					$.InputCalendar({name: 'test'})
 				]),
 				$.Box({route: 'cmp:checkbox'})
