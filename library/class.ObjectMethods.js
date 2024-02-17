2de3e71be6d0a49640fac476c9208c476ebfb7b3
Alexander <Alexander@MacBook-Pro-4.local>
2024-02-17 09:06:37 +0200

    select

--- library/class.ObjectMethods.js
+++ library/class.ObjectMethods.js
@@ -6,6 +6,27 @@ class ObjectMethods {
 	static deepCopy(o) {
 		return JSON.parse(JSON.stringify(o))
 	}
+
+	static select(o, ss) {
+		ss.split && (ss = ss.split('.'))
+		const f = ObjectMethods.select
+		let r = []
+		if (ss.length) {
+			const s = ss.shift()
+			if (s === '*') {
+				for (const k in o) {
+					r = r.concat(f(o[k], [... ss]))
+				}
+			} else {
+				r = f(o[s], [... ss])
+				if (r === undefined)        { r = []  }
+				else if (!Array.isArray(r)) { r = [r] }
+			}
+		} else {
+			r = o
+		}
+		return r
+	}
 }
 
 export default ObjectMethods
