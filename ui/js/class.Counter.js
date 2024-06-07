c62cf59010a1436bc21ab2f59707daa9e872d8e5
Oleksii Zaichenko <oleksii@shipxdata.com>
2024-06-07 22:00:25 +0300

    update counter

--- ui/js/class.Counter.js
+++ ui/js/class.Counter.js
@@ -6,6 +6,7 @@ export default class Counter extends $.Component {
 	constructor(... args) {
 		super(... args)
 		this.props.default('count', 0)
+		this.props.default('onCountChange', null)
 		this.on($.Event.COUNTER_UPDATE, this._onCounterUpdate)
 	}
 
@@ -17,6 +18,7 @@ export default class Counter extends $.Component {
 		($.isNumber(count) && count > 0) || ($.isString(count) && count !== '')
 			? this.show()
 			: this.hide()
+		this.props.onCountChange && this.props.onCountChange(count)
 	}
 
 	render() {
