2876c441167447c39b0f81c5342421fe7d4610e6
Oleksii Zaichenko <o5zaichenko@gmail.com>
2024-09-04 11:11:23 +0300

    routing documentation

--- core/PuerRouter.js
+++ core/PuerRouter.js
@@ -37,19 +37,26 @@ export default class PuerRouter {
 
 	/********************** PRIVATE ***********************/
 
-	/**
+	/** 
+	 * For the given paths returns components to activate.
 	 * @private
-	 * @param   {Array<String>} paths - Array of paths
+	 * @param   {Array<String>} activePaths - Array of paths
 	 * @return  {Set<PuerComponent>}  - Set of active components
 	 */
-	_getActiveComponents(paths) {
+	_getActiveComponents(activePaths) {
 		let parents = []
-		for (const path of paths) {
+		for (const path of activePaths) {
 			parents = parents.concat(this.paths[path].component.getParents())
 		}
 		return new Set(parents)
 	}
 
+	/**
+	 * Gets the current hash of the page and parses its query string.
+	 * Sets prop query.
+	 * @private
+	 * @return {String} - The hash value
+	 */
 	_getCurrentHash() {
 		let hash = window.location.hash.split('#')[1]
 		let query = ''
@@ -63,7 +70,7 @@ export default class PuerRouter {
 	}
 
 	/**
-	 * Updates the current hash of the page.
+	 * Updates the current hash and hash query of the page.
 	 * @private
 	 * @param   {String} hash  - The hash value
 	 * @param   {Object} query - The query string object
@@ -78,10 +85,9 @@ export default class PuerRouter {
 
 	/**
 	 * Get all matching absolute paths for the given hash.
-	 * 
 	 * @private
-	 * @param   {String} hash - The hash to match
-	 * @return  {Object}      - The matched route
+	 * @param   {String} hash   - The hash to match
+	 * @return  {Array<String>} - The matched paths keys
 	 */
 	_match(hash) {
 		this.tree  = new Route(this.app)
@@ -91,17 +97,25 @@ export default class PuerRouter {
 	}
 
 	/**
+	 * Activates components of active component set and deactivate others.
+	 * Temporarily sets $.isRouting to true.
 	 * @private
 	 * @param   {Array<String>} paths - The matched paths
 	 */
 	_route(paths) {
-		console.log('_route', paths)
 		$.isRouting = true
 		this.app.__route(this._getActiveComponents(paths))
 		$.isRouting = false
 	}
 
-	_foo(hash) {
+	/**
+	 * Tries to route to given hash.
+	 * If no route is found, logs a warning and falls back to default route.
+	 * If the hash is not normalized, updates the hash.
+	 * @private
+	 * @param   {String} hash - The hash to route to
+	 */
+	_engage(hash) {
 		let paths = this._match(hash)
 		if (paths.length === 0) {
 			console.warn(`No path found for "${hash}"`)
@@ -115,28 +129,55 @@ export default class PuerRouter {
 		}
 	}
 
+	/**
+	 * Listens to hashchange event and routes to the new hash.
+	 * @private
+	 * @param {Event} e - The hashchange event
+	 */
 	_onHashChange(e) {
-		this._foo(this._getCurrentHash())
+		this._engage(this._getCurrentHash())
 	}
 
 	/*********************** PUBLIC ***********************/
 
+	/**
+	 * Called from components.
+	 * Commands navigation start.
+	 * @param {String} hash  - The relative hash to route to
+	 * @param {Object} query - The query object to set
+	 */
 	navigate(hash, query=null) {
 		if (!this.queue.isDone()) {
 			this.queue.enqueue(this.navigate, this, [hash, query]).start()
 		} else {
-			this._foo(hash)
+			this._engage(hash)
 		}
 	}
 
+	/**
+	 * Checks if a query value matches the given value.
+	 * @param  {String} name  - The query parameter name
+	 * @param  {String} value - The value to check
+	 * @return {Boolean}      - True if the value matches
+	 */
 	hasQueryValue(name, value) {
 		return this.query[name] === value
 	}
 
+	/**
+	 * Returns the value of the given query parameter.
+	 * @param  {String} name - The query parameter name
+	 * @return {String}      - The value of the query parameter
+	 */
 	getQueryValue(name) {
 		return this.query[name]
 	}
 
+	/**
+	 * Initializes the router.
+	 * Adds the hashchange event listener and navigates to the current hash.
+	 * Call this method when application is initialized.
+	 */
 	start() {
 		window.addEventListener('hashchange', this._onHashChange.bind(this))
 		const hash = this._getCurrentHash()
